29b8d722c52de16f93fc3a8978f3892cf6852466
Phillip Littman <srpg4ever@gmail.com>
2018-03-26 09:51:20 -0500

    updated references to link to github pages

--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -2,15 +2,10 @@ import React from "react";
 import "./FriendCard.css";
 
 const FriendCard = props => (
-  <div className="card"onClick={() => props.clickFriend(props.id)}>
+  <div className="card" onClick={() => props.clickCount(props.id)}>
     <div className="img-container">
       <img alt={props.name} src={props.image} />
     </div>
-    <div className="content">
-        {props.name}
-    </div>
-    <span >
-    </span>
   </div>
 );
 
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Title.css";
 
-const Title = props => <h1 className="title">{props.children}</h1>;
-
+const Title = props => (
+    <div className="titleBox">
+        <div className="title">{props.children}</div>
+        <div className="scores">Current Score: {props.score} High Score: {props.highscore}</div>
+    </div>
+);
 export default Title;
